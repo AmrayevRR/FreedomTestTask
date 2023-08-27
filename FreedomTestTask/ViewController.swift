37bbcc694073cf9475a40b4c3ac9e906c80a552d
RAmrayev <RAmrayev@one.kz>
2023-08-27 12:02:24 +0600

    chore: first commit

---
+++ FreedomTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreedomTestTask
+//
+//  Created by Ramir Amrayev on 27.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
